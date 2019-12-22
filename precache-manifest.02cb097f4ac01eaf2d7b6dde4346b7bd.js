a0b26fc2d8bca84f5d5537af1923acaffd456aaf
Alok Upadhya <alokupadhya0@gmail.com>
2019-12-22 22:03:00 +0530

    Updates

---
+++ precache-manifest.02cb097f4ac01eaf2d7b6dde4346b7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea51fa880dd21cd434242323c93906c2",
+    "url": "/SEOHealth/index.html"
+  },
+  {
+    "revision": "4c2aeaf79da82c4a3135",
+    "url": "/SEOHealth/static/css/main.3d85425e.chunk.css"
+  },
+  {
+    "revision": "b94d582f8d488194a27a",
+    "url": "/SEOHealth/static/js/2.73d632a1.chunk.js"
+  },
+  {
+    "revision": "dae2a6f9fe909195a753b89a54ddd86b",
+    "url": "/SEOHealth/static/js/2.73d632a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c2aeaf79da82c4a3135",
+    "url": "/SEOHealth/static/js/main.6e4dfef7.chunk.js"
+  },
+  {
+    "revision": "fa6d74111fc086cd4295",
+    "url": "/SEOHealth/static/js/runtime-main.b211e244.js"
+  },
+  {
+    "revision": "b30ce43522e41508f53a87950e2dc6d7",
+    "url": "/SEOHealth/static/media/seo-banner.b30ce435.png"
+  }
+]);
